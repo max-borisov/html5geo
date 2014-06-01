82775f57c13efffd7cd92a5af65ae514458f7dee
max-borisov <max.borisov@yahoo.com>
2014-06-01 21:12:28 +0300

    up

--- geo.js
+++ geo.js
@@ -1,20 +1,31 @@
 var Geo = {
 
     location: function() {
-        alert(123);
-
-        /*if (navigator.geolocation) {
-            var timeoutVal = 10 * 1000 * 1000;
+        var that = this;
+        if (navigator.geolocation) {
+//            var timeoutVal = 10 * 1000 * 1000;
             navigator.geolocation.getCurrentPosition(
-                displayPosition,
-                displayError,
-                { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+                that._displayPosition,
+                that._displayError
             );
         }
         else {
             alert("Geolocation is not supported by this browser");
-        }*/
+        }
+    },
+
+    _displayPosition: function(position) {
+//        alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude)
+        $('#lat').html(position.coords.latitude);
+        $('#lng').html(position.coords.longitude);
+    },
+
+    _displayError: function(error) {
+        var errors = {
+            1: 'Permission denied',
+            2: 'Position unavailable',
+            3: 'Request timeout'
+        };
+        alert("Error: " + errors[error.code]);
     }
-
-
 }
