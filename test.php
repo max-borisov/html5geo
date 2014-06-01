4b9b83fdc2c98f464d6a7eb8ecf6a060094db311
max-borisov <max.borisov@yahoo.com>
2014-06-01 20:56:22 +0300

    up

--- test.php
+++ test.php
@@ -1,2 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="geo.js"></script>
+</head>
+<body>
+
+<script type="text/javascript">
+    $(function() {
+        Geo.location();
+    })
+</script>
+
+<div id="lat"></div>
+<div id="lng"></div>
+
+<script type="text/javascript">
+
+</script>
+
 <?php
-echo 'test';
+echo time();
+?>
+
+</body>
+</html>
